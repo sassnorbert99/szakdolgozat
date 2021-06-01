ad29429b793311bd8a14894c2eef00ee2d6dcc33
Sass-Gyarmati Norbert <sassnorbert99@gmail.com>
2021-06-01 20:43:34 +0200

    abouts oldalon jelszavak és hozzáférési tényezők törölve

--- Weblap/application/controllers/Abouts.php
+++ Weblap/application/controllers/Abouts.php
@@ -11,7 +11,7 @@
 			    $subject = $_POST["subject"];
 			    $content = $_POST["content"];
 
-			    $conn = mysqli_connect("89.40.126.232:8080", "varazstorony", "VarazsTorony_18", "varazstorony") or die("Connection Error: " . mysqli_error($conn));
+			    $conn = mysqli_connect("", "", "", "") or die("Connection Error: " . mysqli_error($conn));
 			    mysqli_query($conn, "INSERT INTO contact (user_name, user_email,subject,content) VALUES ('" . $name. "', '" . $email. "','" . $subject. "','" . $content. "')");
 			    $insert_id = mysqli_insert_id($conn);
 			    //if(!empty($insert_id)) {
